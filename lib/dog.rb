9bbf003cd9e0260bdefe399134876beebdb7af28
apolito117 <anthonyalexanderpolito@gmail.com>
2020-08-04 12:02:49 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+# Defining dog class below
+class Dog
+end
+
+fido = Dog.new
+
+snoopy = Dog.new
+
+lassie = Dog.new
+
+
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+# Defining Person class below
+class Person
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
+
